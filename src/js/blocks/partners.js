36a94c3794ce6738bd5b9c3dbdcb6a8259488ba0
vertalet76crew <vertalet76crew@gmail.com>
2020-12-28 11:08:21 +0400

    redesign

--- src/js/blocks/partners.js
+++ src/js/blocks/partners.js
@@ -3,45 +3,41 @@ import 'owl.carousel';
 
 function partnersCarousel() {
     const $slider = $('[data-carousel="partners"]');
-
-    if ($slider.length) {
-        $slider.owlCarousel({
-            nav: false,
-            dots: true,
-            smartSpeed: 2000,
-            startPosition: 1,
-            loop: true,
-            fluidSpeed: 2000,
-            dotsSpeed: 2000,
-            dragEndSpeed: 1000,
-            responsive: {
-                320: {
-                    autoWidth: false,
-                    responsiveClass: true,
-                    margin: 20,
-                    items: 1,
-                    center: true,
-                    loop: false
-                },
-                1020: {
-                    autoWidth: false,
-                    responsiveClass: true,
-                    margin: 35,
-                    items: 3,
-                    center: true,
-                    loop: true
-                },
-                1360: {
-                    autoWidth: false,
-                    responsiveClass: true,
-                    items: 3,
-                    center: true,
-                    margin: 47,
-                    loop: true
-                }
+    const options = {
+        nav: false,
+        dots: true,
+        smartSpeed: 2000,
+        startPosition: 0,
+        fluidSpeed: 2000,
+        dotsSpeed: 2000,
+        dragEndSpeed: 1000,
+        autoWidth: false,
+        responsiveClass: true,
+        margin: 20,
+        items: 1,
+        center: false,
+        loop: false,
+        responsive: {
+            767: {
+                autoWidth: true
             }
-        });
-    }
+        }
+    };
+
+    $(window).on('load resize', function() {
+        const checkWidth = $(window).width();
+        if (checkWidth > 1019) {
+            if (typeof $slider.data('owl.carousel') != 'undefined') {
+                $slider.data('owl.carousel').destroy();
+            }
+            $slider.removeClass('owl-carousel');
+            $slider.addClass('owl-off');
+        } else if (checkWidth < 1020) {
+            $slider.addClass('owl-carousel');
+            $slider.owlCarousel(options);
+            $slider.removeClass('owl-off');
+        }
+    });
 }
 
 export default partnersCarousel;
